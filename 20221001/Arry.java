bee9b9d43adfd5416e6440a955b0745645f8b41d
hacker2889 <3331497264@qq.com>
2022-10-04 14:19:45 +0800

    dayFour

---
+++ 20221001/Arry.java
@@ -0,0 +1,72 @@
+//import java.util.Scanner;
+
+public class Arry {
+    public static void main(String[] args) {
+//        int sum = 0;
+//        for (int i = 0; i <= 100; i++) {
+//            for (int j = 0; j <= i; j++) {
+//                sum += j;
+//            }
+//        }
+//        double[] a = {2.3, 14, 15, 0.3, 45, 48, 77};
+//        System.out.println(a.length);
+//        System.out.println("sum=" + sum);
+
+//        double scores[] = new double[5];
+//        Scanner myScanner = new Scanner(System.in);
+//        for (int i = 0; i < scores.length; i++) {
+//            System.out.println("请输入第" + (i + 1) + "个元素");
+//            scores[i] = myScanner.nextDouble();
+//        }
+//        for (int i = 0; i < scores.length; i++) {
+//            System.out.println("第" + (i + 1) + "个元素的值="+scores[i]);
+//        }
+//        char[] arr = new char[26];
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = (char)('A' + i);
+//            System.out.print(arr[i]+" ");
+//        }
+//
+        //定义一个字符串数组
+//        String[] names = {"洛克李", "赤砂之蝎", "卡卡西", "我爱罗" };
+//        Scanner myScanner = new Scanner(System.in);
+//
+//        System.out.println("请输入名字");
+//        String findName = myScanner.next();
+//        //遍历数组,逐一比较,如果有,提示信息,并退出
+//        //
+//        int index = -1;
+//        for (int i = 0; i < names.length; i++) {
+//            //比较字符串 equals
+//            if (findName.equals(names[i])) {
+//                System.out.println("恭喜你找到"+ findName);
+//                System.out.println("下标为" + i);
+//                //把i保存到index
+//                index = i;
+//                break;
+//            }
+//        }
+//        if(index==-1){
+//            System.out.println("没有找到");
+//        }
+//        int[] arr = new int[10];
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = (int) (Math.random() * 100) + 1;
+//        }
+//        System.out.println("========arr的元素情况===========");
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i] + "\t");
+//        }
+//        System.out.println("\n======arr的元素情况(倒序)======");
+//        for (int i = arr.length - 1; i >= 0; i--) {
+//            System.out.print(arr[i] + "\t");
+//        }
+//        int i = 0;
+//        int j = 5;
+//        if (j == 5) {
+//            for (; i < 5; i++) {
+//                System.out.println("呵呵");
+        //  }
+
+    }
+}
