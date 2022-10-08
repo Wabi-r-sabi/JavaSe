92fb77c8b0aeed29666953d54197c39a039e16a0
hacker2889 <3331497264@qq.com>
2022-10-08 15:05:10 +0800

    dayFive

---
+++ 20221001/Recursion.java
@@ -0,0 +1,68 @@
+/**
+ * @descriptions: Test3
+ * @author: 一只强
+ * @date: 2022/10/4 22:22
+ * @version: 1.0
+ */
+public class Recursion {
+    public static void main(String[] args) {
+//        Tt t1 = new Tt();
+        //测试
+//        t1.test(4);
+        //阶乘
+//        int res = t1.factorial(5);
+//        System.out.println("5的阶乘为:" + res);
+        //斐波那契额数
+//        int n = 30;
+//        int res = t1.fibonacci(n);
+//        System.out.println("第" + n + "个斐波那契额数为" + res);
+        //猴子吃桃
+//        int day = 5;
+//        int peachNum = t1.peach(day);
+//        if (peachNum != -1) {
+//            System.out.println("第" + day + "天有" + peachNum + "个桃子");
+//        }
+    }
+}
+
+class Tt {
+    /**
+     * public void test(int n) {
+     * if (n > 2) {
+     * test(n - 1);
+     * }
+     * System.out.println(n);
+     * }
+     * <p>
+     * public int factorial(int n) {
+     * if (n == 1) {
+     * return 1;
+     * } else {
+     * return factorial(n - 1) * n;
+     * }
+     * }
+     * <p>
+     * public int fibonacci(int n) {
+     * if (n >= 1) {
+     * if (n == 1 || n == 2) {
+     * return 1;
+     * } else {
+     * return fibonacci(n - 1) + fibonacci(n - 2);
+     * }
+     * } else {
+     * System.out.println("请输入n>=1的值");
+     * return -1;
+     * }
+     * }
+     public int peach(int day) {
+     if (day == 10) {
+     return 1;
+     } else if (day >= 1 && day <= 9) {
+     return (peach(day + 1) + 1) * 2;
+     } else {
+     System.out.println("day在1-10");
+     return -1;
+     }
+     }
+     */
+}
